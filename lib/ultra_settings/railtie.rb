e5aa7d1786d3a8518bdc12ed648ca688a105403d
Brian Durand <bbdurand@gmail.com>
2023-06-12 21:55:32 -0700

    add rails initialization

--- lib/ultra_settings/railtie.rb
+++ lib/ultra_settings/railtie.rb
@@ -2,22 +2,26 @@
 
 module UltraSettings
   class Railtie < Rails::Railtie
+    config.ultra_settings = ActiveSupport::OrderedOptions.new
+    config.ultra_settings.auto_load_directory = File.join("app", "configurations")
+
     config.before_configuration do
       UltraSettings.yaml_config_env = Rails.env
       UltraSettings.yaml_config_path = Rails.root.join("config")
     end
 
+    # Automatically register any configuration classes in the app/configurations
+    # directory. The path to load can be customized by setting the
+    # `config.ultra_settings.auto_load_directory` option.
     config.after_initialize do
-      # TODO: check if config has not been disabled. Then load load
-      # all classes found in app/configurations directory. Should also
-      # allow for a custom directory to be specified.
-      #
-      # app_config_dir = File.expand_path('../app/configurations', __dir__)
-      # Dir.glob("#{app_config_dir}/**/*.rb").each do |file_path|
-      #   config_name = File.basename(file_path, '.rb')
-      #   class_name = file_path[(app_config_dir.length + 1)..-4].classify
-      #   UltraSettings.add(config_name, class_name)
-      # end
+      if Rails.application.config.ultra_settings.auto_load
+        app_config_dir = Rails.root.join(config.ultra_settings.auto_load_directory)
+        app_config_dir.glob("**/*.rb").each do |file_path|
+          config_name = file_path.basename(".rb")
+          class_name = file_path.relative_path_from(app_config_dir).to_s.chomp(".rb").classify
+          UltraSettings.add(config_name, class_name)
+        end
+      end
     end
   end
 end
