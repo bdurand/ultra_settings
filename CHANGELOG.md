ba7e7fdc950552b01d7e7f7ba02df0c0c365d676
Brian Durand <bbdurand@gmail.com>
2024-09-17 16:17:43 -0700

    Add support for deep links

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,16 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## 1.1.1
+
+### Added
+
+- Support for deep linking to a specific configuration section in the web UI. This is done by adding a query fragment to the URL matching the configuration name.
+
+### Changed
+
+- Changed the title on the Web UI to "Application Configuration" to better match the semantics of configuration objects.
+
 ## 1.1.0
 
 ### Added
--- app/application.js
+++ app/application.js
@@ -3,10 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
   showCurrentConfiguration = () => {
     const selectedId = menu.options[menu.selectedIndex].value;
+    const hash = selectedId.replace(/^config-/, "");
 
     document.querySelectorAll(".ultra-settings-configuration").forEach((configuration) => {
       if (configuration.id === selectedId) {
         configuration.style.display = "block";
+        window.location.hash = hash;
       } else {
         configuration.style.display = "none";
       }
@@ -14,6 +16,26 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   menu.addEventListener("change", showCurrentConfiguration);
+
+  const setCurrentSelection =  () => {
+    const hash = window.location.hash.replace(/^#/, "");
+    const selectedId = `config-${hash}`;
+    for (const option of menu.options) {
+      if (option.value === selectedId) {
+        option.selected = true;
+        break;
+      }
+    }
+
+    showCurrentConfiguration();
+  }
+
+  window.addEventListener('hashchange', setCurrentSelection);
+
+
+  if (window.location.hash) {
+    setCurrentSelection()
+  }
 
   showCurrentConfiguration();
 });
