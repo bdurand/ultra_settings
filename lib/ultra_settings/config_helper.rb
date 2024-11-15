a354bbbbff5a496a1e35bac0a19541cfd7306dd8
Samer Masry <samer.masry@gmail.com>
2024-11-15 10:32:59 -0800

    Adding config helper module to add config methods to access the configuration

---
+++ lib/ultra_settings/config_helper.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module UltraSettings
+  # Helper module for setting up a class to use the config methods
+  # Usage
+  # class TestClass
+  #   extend ConfigHelper
+  #   configuration_class TestConfiguration
+  # end
+  # TestClass.config => TestConfiguration.instance
+  # TestClass.new.config => TestConfiguration.instance
+  module ConfigHelper
+    def configuration_class(config_class)
+      define_singleton_method :config do
+        config_class.instance
+      end
+
+      define_method :config do
+        self.class.config
+      end
+    end
+  end
+end
