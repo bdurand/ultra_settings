3f428c38a584513c090199720cfea6b6714a30f3
Jeremy Park <jeremyjpark@gmail.com>
2026-02-24 15:59:35 -0800

    Add config_alias to configuration_class
    When you need to reference multiple configurations and use the default 'config' helper method, they end up clobbering each other. This change should allow the usage of a different helper method.

--- spec/ultra_settings/config_helper_spec.rb
+++ spec/ultra_settings/config_helper_spec.rb
@@ -4,12 +4,16 @@ require_relative "../spec_helper"
 
 RSpec.describe UltraSettings::ConfigHelper do
   subject(:service_class) do
+    alternative_name = config_alias
+
     Class.new do
       extend UltraSettings::ConfigHelper
 
-      configuration_class TestConfiguration
+      configuration_class TestConfiguration, config_alias: alternative_name
     end
   end
+
+  let(:config_alias) { :config }
 
   describe "config" do
     it "should return the configuration" do
@@ -20,6 +24,22 @@ RSpec.describe UltraSettings::ConfigHelper do
   describe "self.config" do
     it "should return the configuration" do
       expect(service_class.new.config).to eq(TestConfiguration.instance)
+    end
+  end
+
+  context "when the config alias is test_config" do
+    let(:config_alias) { :test_config }
+
+    describe "test_config" do
+      it "should return the configuration" do
+        expect(service_class.test_config).to eq(TestConfiguration.instance)
+      end
+    end
+
+    describe "self.test_config" do
+      it "should return the configuration" do
+        expect(service_class.new.test_config).to eq(TestConfiguration.instance)
+      end
     end
   end
 end
